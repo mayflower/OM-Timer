d2c6e1760d10d24681efe9e376830e05fb2e0871
Florian Fackler <florian.fackler@mintao.com>
2016-09-26 12:39:44 +0200

    Add initial filled timerSets and reset

--- www/js/controllers/appController.js
+++ www/js/controllers/appController.js
@@ -1,15 +1,15 @@
 !function (app) {
   'use strict';
   app.controller('AppController', [
-    '$rootScope', 'Media', '$scope', 'localStorageService',
-    function ($rootScope, Media, $scope, localStorageService) {
+    '$rootScope', 'Media', '$scope', 'localStorageService', 'initialTemplate',
+    function ($rootScope, Media, $scope, localStorageService, initialTemplate) {
       $rootScope.$on('sound', function (event, phase) {
         var sound = phase.alarmSound;
         // var selfStopping = phase.selfStoppingSound;
         Media.play(sound);
       });
 
-      $scope.unbindLocalStorage = localStorageService.bind($scope, 'timerSets', [])
+      $scope.unbindLocalStorage = localStorageService.bind($scope, 'timerSets', initialTemplate)
 
     }]);
 }(window.angular.module('omtimer'));
--- www/js/controllers/resetController.js
+++ www/js/controllers/resetController.js
@@ -1,12 +1,16 @@
 !function (app) {
   'use strict';
   app.controller('ResetController', [
-    '$state', 'initialTemplate', '$window', 'confirmation', '$scope', 'localStorageService',
-    function ($state, initialTemplate, $window, confirmation, $scope, localStorageService) {
+    '$state', 'initialTemplate', '$window', 'confirmation', '$scope',
+    function ($state, initialTemplate, $window, confirmation, $scope) {
       if (confirmation === 'no' || confirm('Really reset all data?')) {
-        $scope.unbindLocalStorage();
-        localStorageService.set('timerSets', initialTemplate);
-        $window.location.href = '/';
+        $scope.timerSets.splice(0, $scope.timerSets.length);
+        for (var i in initialTemplate) {
+          if (initialTemplate.hasOwnProperty(i)) {
+            $scope.timerSets.push(initialTemplate[i]);
+          }
+        }
+        $state.go('app.timerSets', {}, {reload: true, location: 'replace'});
       }
     }]
   );
