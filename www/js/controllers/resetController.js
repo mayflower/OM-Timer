08d137f948a342e511a9caaf33e9588d844302ee
Florian Fackler <florian.fackler@mintao.com>
2016-09-26 12:46:23 +0200

    Disable reset due to buggy behavior

--- www/js/controllers/resetController.js
+++ www/js/controllers/resetController.js
@@ -5,12 +5,13 @@
     function ($state, initialTemplate, $window, confirmation, $scope) {
       if (confirmation === 'no' || confirm('Really reset all data?')) {
         $scope.timerSets.splice(0, $scope.timerSets.length);
+        // Don't loose two way data binding. So don't simply overwrite $scope.timerSets
         for (var i in initialTemplate) {
           if (initialTemplate.hasOwnProperty(i)) {
             $scope.timerSets.push(initialTemplate[i]);
           }
         }
-        $state.go('app.timerSets', {}, {reload: true, location: 'replace'});
+        $state.go('app.timerSets', {}, {reload: true});
       }
     }]
   );
